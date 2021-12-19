387e9b8591f3ee18bb77c62c46937ea710ea2be3
Wilson <wilson.delgadoher@gmail.com>
2021-12-19 17:14:04 -0500

    models_tweets

---
+++ main.py
@@ -0,0 +1,53 @@
+# Python
+from typing import Optional
+from uuid import UUID
+from datetime import date, datetime
+from typing import Optional
+# Pydantic
+from pydantic import BaseModel, EmailStr, Field
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50)
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50)
+    birthday: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    Tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=280
+        )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
+
+
+@app.get(
+    path = '/'
+)
+def home():
+    return {"Twitter API": "Working"}
